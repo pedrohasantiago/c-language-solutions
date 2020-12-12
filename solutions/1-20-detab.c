6b49bffe0fc0243aa0d68fbca3533c96343271b8
Pedro Santiago <pedrohasantiago@gmail.com>
2020-12-12 14:00:50 -0300

    detab working

--- solutions/1-20-detab.c
+++ solutions/1-20-detab.c
@@ -4,54 +4,29 @@
 
 #include <stdio.h>
 
-#define MAX_LINE_LEN 1000
 #define TAB_STOP_SIZE 10 // Size in columns for each tab stop
 #define DEBUG 0
 
-int detab(char[], int, char[]);
-int getLine(char[], int);
-
 int main() {
-    char currLine[MAX_LINE_LEN];
-    char detabbedLine[MAX_LINE_LEN];
-    int len, i;
-    while (getLine(currLine, MAX_LINE_LEN) > 0) {
-        len = detab(currLine, TAB_STOP_SIZE, detabbedLine);
-        for (i = 0; i < len; ++i)
-            putchar(detabbedLine[i]);
-    }
-}
-
-int detab(char input[], int tabStopSize, char output[]) {
-    int i, c, j, toAdd, outputPos;
-    int extraSpaces = 0;
-    for (i = 0; (c = input[i]) != '\0'; ++i) {
-        outputPos = i + extraSpaces;
-	/* if (DEBUG) printf("i %d outputPos %d extraSpaces %d\n", i, outputPos, extraSpaces); */
+    int i = 0;
+    int c, blanksToAdd, j;
+    while ((c = getchar()) != EOF) {
+        if (DEBUG) printf("%d ", i);
         if (c == '\t') {
-            toAdd = outputPos / tabStopSize * tabStopSize + tabStopSize - outputPos;
-	    if (DEBUG) printf("Replacing \\tab with %d spaces\n", toAdd);
-            for (j = 0; j < toAdd; ++j) {
-	        if (DEBUG) printf("%d is whitespace\n", j + outputPos);
-                output[j + outputPos] = ' ';
-                if (j > 0)
-                    ++extraSpaces;
+            blanksToAdd = i / TAB_STOP_SIZE * TAB_STOP_SIZE + TAB_STOP_SIZE - i;
+            if (DEBUG) printf("is \\t. Adding %d blanks\n", blanksToAdd);
+            for (j = 0; j < blanksToAdd; ++j) {
+                putchar(' ');
+                ++i;
             }
-        } else
-	    if (DEBUG) printf("%d is char %c\n", outputPos, c);
-            output[outputPos] = c;
+        } else if (c == '\n') {
+            if (DEBUG) printf("is \\n\n");
+            putchar(c);
+            i = 0;
+        } else {
+            if (DEBUG) printf("is %c\n", c);
+            putchar(c);
+            ++i;
+        }
     }
-    return outputPos + 1;
-}
-
-int getLine(char output[], int limit) {
-    int c, i;
-    for (i = 0; (i < (limit - 1)) && ((c = getchar()) != EOF) && (c != '\n'); ++i)
-        output[i] = c;
-    if (c == '\n') {
-        output[i] = c;
-        ++i;
-    }
-    output[i] = '\0';
-    return i;
 }
